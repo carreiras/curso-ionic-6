11df9b03591a0f98700f7aa3416a37c2f6e32c07
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-02-22 17:58:49 -0300

    components baseados em tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent {
     { title: 'Alert', url: 'alert', icon: 'cube' },
     { title: 'Button', url: 'button', icon: 'cube' },
     { title: 'Card', url: 'card', icon: 'cube' },
+    { title: 'Components - Js', url: 'js', icon: 'cube' },
+    { title: 'Components - Tags', url: 'tags', icon: 'cube' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
