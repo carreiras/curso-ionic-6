113eba57e337fd26e843ed3d5dd3ac24c5e40aec
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-02-22 08:22:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'curso-ionic-6',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
